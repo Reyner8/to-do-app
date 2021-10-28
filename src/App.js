750de35cb0c92a4bccf90a76d77d87cbd634c722
reyner <48514029+Reyner8@users.noreply.github.com>
2021-10-28 12:00:31 +0800

    to-do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ContentBox from "./component/ContentBox";
+import FormInput from "./component/FormInput";
+import Task from "./component/Task";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskList: [],
+    };
+  }
+
+  taskEvent = (newTask) => {
+    this.setState({
+      taskList: [...this.state.taskList, newTask],
+    });
+  };
+
+  removeEvent = (task) => {
+    const tasks = this.state.taskList;
+    const filtered = tasks.filter((t) => {
+      return t.id != task.id;
+    });
+    this.setState({ taskList: filtered });
+  };
+
+  doneTask = (task) => {
+    if (task.isDone == false) {
+      // change isDone to true
+      const updateTask = { id: task.id, task: task.task, isDone: true };
+      const tasks = this.state.taskList.filter((t) => {
+        return t.id != task.id;
+      });
+
+      this.setState({ taskList: [...tasks, updateTask] });
+    } else {
+      // change isDone to false
+      const updateTask = { id: task.id, task: task.task, isDone: false };
+      const tasks = this.state.taskList.filter((t) => {
+        return t.id != task.id;
+      });
+      this.setState({ taskList: [...tasks, updateTask] });
+    }
+  };
+
+  clearTasks = () => {
+    this.setState({ taskList: [] });
+  };
+
+  render() {
+    // map task
+    const list = this.state.taskList.map((val) => {
+      return (
+        <Task
+          key={val.id}
+          content={val}
+          remove={this.removeEvent}
+          checkBox={this.doneTask}
+        />
+      );
+    });
+
+    return (
+      <div className="container">
+        <div className="title">
+          <h1 className="app-name">To-do App</h1>
+        </div>
+
+        <div className="todo-box">
+          <FormInput event={this.taskEvent} />
+          <ContentBox clear={this.clearTasks}>{list}</ContentBox>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
